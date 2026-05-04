ab337bcf795a587becc297557acc8b37c8155bbd
Raman <ramanbisht77@gmail.com>
2026-05-04 15:32:58 +0530

    resolved importing

--- src/assets/AllProduct.js
+++ src/assets/AllProduct.js
@@ -1,4 +1,3 @@
-// import kurti from './kurti.webp'
 import jeans from './jeans.webp'
 import saree from './saree.avif'
 import jacket from './jacket.webp'
@@ -24,15 +23,6 @@ import kid7 from './kid7.webp'
 import kid8 from './kid8.png'
 
 let all_product = [
-    //   {
-    //         id: 1,
-    //         name: "Kurti",
-    //         category: "women",
-    //         image: kurti,
-    //         new_price: 50.0,
-    //         old_price: 80.5
-    //     },
-    
     { id: 2, name: "Jeans", category: "women", image:jeans, new_price: 60.0, old_price: 90.5 },
     { id: 3, name: "Saree", category: "women", image:saree, new_price: 75.0, old_price: 120.0 },
     { id: 4, name: "Jacket", category: "women", image: jacket, new_price: 90.0, old_price: 140.0 },
