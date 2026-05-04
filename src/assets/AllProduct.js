e1855e4e2432e51136aab245a3b2ae969e2773f9
Raman <ramanbisht77@gmail.com>
2026-05-04 15:18:16 +0530

    resolved importing

--- src/assets/AllProduct.js
+++ src/assets/AllProduct.js
@@ -1,4 +1,4 @@
-import kurti from './kurti.webp'
+// import kurti from './kurti.webp'
 import jeans from './jeans.webp'
 import saree from './saree.avif'
 import jacket from './jacket.webp'
@@ -24,14 +24,14 @@ import kid7 from './kid7.webp'
 import kid8 from './kid8.png'
 
 let all_product = [
-      {
-            id: 1,
-            name: "Kurti",
-            category: "women",
-            image: kurti,
-            new_price: 50.0,
-            old_price: 80.5
-        },
+    //   {
+    //         id: 1,
+    //         name: "Kurti",
+    //         category: "women",
+    //         image: kurti,
+    //         new_price: 50.0,
+    //         old_price: 80.5
+    //     },
     
     { id: 2, name: "Jeans", category: "women", image:jeans, new_price: 60.0, old_price: 90.5 },
     { id: 3, name: "Saree", category: "women", image:saree, new_price: 75.0, old_price: 120.0 },
--- src/assets/data.js
+++ src/assets/data.js
@@ -1,17 +1,17 @@
-import kurti from '../assets/kurti.webp'
-import jeans from '../assets/jeans.webp'
+// import kurti from './assets/kurti.webp'
+import jeans from './assets/jeans.webp'
 import saree from '../assets/saree.avif'
 import jacket from '../assets/jacket.webp'
 
 let data_product= [
-     {
-            id: 1,
-            name: "Kurti",
-            category: "women",
-            image: kurti,
-            new_price: 50.0,
-            old_price: 80.5
-        },
+    //  {
+    //         id: 1,
+    //         name: "Kurti",
+    //         category: "women",
+    //         image: kurti,
+    //         new_price: 50.0,
+    //         old_price: 80.5
+    //     },
     
         { id: 2, name: "Jeans", category: "women", image:jeans, new_price: 60.0, old_price: 90.5 },
         { id: 3, name: "Saree", category: "women", image:saree, new_price: 75.0, old_price: 120.0 },
